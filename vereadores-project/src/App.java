8cb53a727dd2b1a9b2922c1b1550cece4be54ad8
dmgobbi <daniel.gobbi@edu.ufes.br>
2025-02-06 12:00:11 -0300

    Adicionar classe Candidato e LeitorCSV

--- vereadores-project/src/App.java
+++ vereadores-project/src/App.java
@@ -1,5 +1,12 @@
+import java.util.List;
+
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+        List<Candidato> candidatos = LeitorCSV.lerCandidatos("../consulta_cand_2024/consulta_cand_2024_ES.csv");
+
+        // Exibir os 10 primeiros candidatos lidos para verificação
+        for (int i = 0; i < Math.min(10, candidatos.size()); i++) {
+            System.out.println(candidatos.get(i));
+        }
     }
 }
